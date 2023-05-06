f059c802c23db8242f37cd231b0496398ea1bbb2
AKSUF <abu053125@gmail.com>
2023-05-06 22:31:48 +0600

    arrow function syntics

---
+++ newfile.js
@@ -0,0 +1,26 @@
+// This code declares an arrow function named "number"
+// let number = () => {
+//         return 10;
+//     }
+
+
+
+// Declare a variable `number` and assign an arrow function to it
+// let number = () => 10;
+
+
+// This code declares an arrow function named "number" that takes a parameter "n" and always returns the integer 10.
+// let number = (n) => 10;
+
+//it is allowed not to use parameter in  in single parameter
+// let number = n => 10;
+
+
+//when use double parammeter have to use bracket
+// let number = (a, b) => a + b;
+// let a = number(45, 7);
+
+
+
+console.log(number());
+document.getElementById('demo').innerHTML = a
