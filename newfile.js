9b91d51017f66c4f983b354a0224e05f383760de
AKSUF <abu053125@gmail.com>
2023-05-07 07:42:31 +0600

    understand array.find()

--- newfile.js
+++ newfile.js
@@ -1,17 +1,24 @@
-//ternary operator
-var age = 20;
-let adult = "Above 18"
-let teege = "This is before 18"
-let boy = "this is boy age"
-let child = "This is child age"
-
-
-var type = (age >= 18) ? adult : (age < 10) ? boy : child;
-
-var a = 6;
-var access = a > 5;
-document.getElementById('remo').innerHTML = access;
-
-
-
-document.getElementById('demo').innerHTML = type;
+//array.find()
+var numbers = [1, 2, 3, 4, 5, 6, 7, 8];
+var result = numbers.find(function(currentValue, currentIndex, arr) {
+    return currentValue > 4;
+})
+document.getElementById('remo').innerHTML = result;
+class Student {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+    test() {
+        console.log('hello');
+    }
+    exampleFunction() {
+        letarray = [1, 2, 3, 4];
+        Array.find(() => {
+            this.test();
+        })
+    }
+}
+let student = new Student('sumit', 1);
+student.exampleFunction();
+document.getElementById('demo').innerHTML = student.exampleFunction();
