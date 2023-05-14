65433cc1dc2f24d5a85f77b6a55495356648198f
AKSUF <abu053125@gmail.com>
2023-05-14 23:46:13 +0600

    how to use find method in javascript

--- array.js
+++ array.js
@@ -1,7 +1,6 @@
-//array.prototype
 var numbers = [1, 2, 3, 4, 5, 6, 10];
 var result = numbers.find(function(currentVallue) {
     return currentVallue > 4;
-});
+}, this);
 console.log(numbers);
 console.log(result);
---
+++ class.js
@@ -0,0 +1,18 @@
+class Student {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+    test() {
+        console.log('hello');
+    }
+    exampleFunction() {
+        let array = [1, 2, 3];
+        array.find(() => {
+            this.test();
+        })
+    }
+}
+
+let student = new Student("sumit", 45);
+student.exampleFunction();
